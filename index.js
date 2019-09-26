e25edce97ca9b5418543dfd4318e9a130fbdb858
D Paul Childs <clausewitzian@gmail.com>
2019-09-26 17:15:26 -0400

    working on cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const app = express()
 const parser = require('body-parser')
 const cors = require('cors')
 const passport = require('./config/passport')()
 
+app.use(cors())
+
+app.use(parser.urlencoded({extended: true}))
+app.use(parser.json());
+
 const userController = require('./controllers/users.js')
 const dogController = require('./controllers/dogs.js')
 
-const app = express()
-
-app.use(cors())
 app.use(parser.json())
 app.use(passport.initialize())
 
